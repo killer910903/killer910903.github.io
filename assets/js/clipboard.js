e141036560333f4cdbbd3f0fcf8395b41d2d6ede
killer910903 <67303235+killer910903@users.noreply.github.com>
2024-04-25 04:08:57 +0800

    Add files via upload

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('dream-skyblock.xyz')
+    swal({
+    title: '網站提示',
+    text: 'IP複製成功!',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
